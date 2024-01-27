c0179cbb36aff0e96cf0090150446d3453033f6d
felipmartins <felipmartins94@gmail.com>
2024-01-27 10:44:07 -0300

    refact: overwrite create method

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
 from users.models import SimpleUser
 
+# from config import settings
+from django.http import JsonResponse
+
 
 class SimpleUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = SimpleUser
         fields = "__all__"
+
+    def create(self, validated_data):
+        user_data = {}  # Retrieve user data from the request
+        # producer.produce(
+        #     settings.USER_EVENTS_TOPIC,
+        #     key="user_signup",
+        #     value=validated_data,
+        # )
+        # producer.flush()
+        user = SimpleUser.objects.create(**user_data)
+
+        return JsonResponse(
+            {
+                "message": "User signed up successfully",
+                "user": user,
+            }
+        )
